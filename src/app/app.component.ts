52f0f8693700a3fa59dbc2b574212735ae4ea42a
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2024-07-14 10:15:32 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExploreComponent } from './components/explore/explore.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ExploreComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'health-track';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ExploreComponent } from './components/explore/explore.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddWorkoutComponent } from './components/add-workout/add-workout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'explore',
+    component: ExploreComponent,
+  },
+  {
+    path: 'add-workout',
+    component: AddWorkoutComponent,
+  },
+];
